47e061935ec1ca95f289903c8c8df8e1724d7875
Ubiquity89 <ubiquity89@gmail.com>
2026-04-16 23:50:14 +0530

    fix: correct api urls

--- FrontEnd/src/utils/constant.js
+++ FrontEnd/src/utils/constant.js
@@ -1,8 +1,8 @@
-export const USER_API_END_POINT = "http://https://jobportal-bj6k.onrender.com/api/v1/user";
+export const USER_API_END_POINT = "https://jobportal-bj6k.onrender.com/api/v1/user";
 
-export const JOB_API_END_POINT="http://https://jobportal-bj6k.onrender.com/api/v1/job";
+export const JOB_API_END_POINT="https://jobportal-bj6k.onrender.com/api/v1/job";
 
-export const APPLICATION_API_END_POINT="http://https://jobportal-bj6k.onrender.com/api/v1/application";
+export const APPLICATION_API_END_POINT="https://jobportal-bj6k.onrender.com/api/v1/application";
 
-export const COMPANY_API_END_POINT="http://https://jobportal-bj6k.onrender.com/api/v1/company";
+export const COMPANY_API_END_POINT="https://jobportal-bj6k.onrender.com/api/v1/company";
  
